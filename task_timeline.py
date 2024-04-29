03d24b1fad35343e4f8733aa3e632073bb5a0c54
Mihailo Isakov <mihailo@boolsi.com>
2024-04-29 14:57:38 +0200

    added options to compare tasks with getting and without getting the results

--- task_timeline.py
+++ task_timeline.py
@@ -54,7 +54,7 @@ def hashloop(b: int, c: int):
     return array
 
 
-def benchmark_ray_tasks_with_getting(b: int, h: int):
+def benchmark_ray_tasks(b: int, h: int, get=True):
     worker = ray.remote(hashloop)
     cpus = int(ray.available_resources()['CPU'])
 
@@ -66,39 +66,29 @@ def benchmark_ray_tasks_with_getting(b: int, h: int):
         for _ in ready_refs:
             ends.append(time() - start)
 
-        _ = ray.get(ready_refs)
+        if get: 
+            _ = ray.get(ready_refs)
 
     return ends
 
 
-def _byte_units(count: int) -> str:
-    if 1024**1 <= count < 1024**2:
-        return f"{count/1024:.2f}KB"
-    if 1024**2 <= count < 1024**3:
-        return f"{count/1024**2:.2f}MB"
-    if 1024**3 <= count < 1024**4:
-        return f"{count/1024**3:.2f}GB"
-    if 1024**4 <= count < 1024**5:
-        return f"{count/1024**4:.2f}TB"
-    else:
-        return f"{count}B"
+def _units(count: int) -> str:
+    if 1024**1 <= count < 1024**2: return f"{count/1024**1:.2f}K"
+    if 1024**2 <= count < 1024**3: return f"{count/1024**2:.2f}M"
+    if 1024**3 <= count < 1024**4: return f"{count/1024**3:.2f}G"
+    if 1024**4 <= count < 1024**5: return f"{count/1024**4:.2f}T"
+    else:                          return f"{count}"
 
 
-def experiment(_bytes, _hashes): 
-    _ = benchmark_ray_tasks_with_getting(2**22, 2**22)  # rev it once 
+def experiment(_bytes, _hashes, get): 
+    _ = benchmark_ray_tasks(2**22, 2**22, get=True)  # rev it once 
     print("Finished revving!")
 
     results = np.zeros((len(_bytes), len(_hashes), 224))
     for b_idx, b in enumerate(tqdm(_bytes)): 
         for h_idx, h in enumerate(tqdm(_hashes, leave=False)):
-            ends = benchmark_ray_tasks_with_getting(b, h)
+            ends = benchmark_ray_tasks(b, h, get)
             results[b_idx, h_idx] = ends
-            plt.plot(ends, label=f"{h} hashes, {_byte_units(b)}, over 244 cores")
-
-    plt.legend()
-    plt.xlabel("Ray task [#]")
-    plt.ylabel("Time [s]")
-    plt.show()
 
     return results
 
@@ -112,28 +102,47 @@ def plot_grid(results, _bytes, _hashes):
         for y in range(cols):
             plt.sca(axes[x, y])
             plt.plot(results[x, y])
-            axes[x, y].set_title(f"{_byte_units(_bytes[x])}, {_hashes[y]} hashes")
+            axes[x, y].set_title(f"{_units(_bytes[x])}B, {_hashes[y]} hashes")
             if y == 0: axes[x, y].set_ylabel("Time [s]")
             if x == rows - 1: axes[x, y].set_xlabel("Ray task [#]")
 
+    plt.show()
+
+
+def plot_two_grids(results1, results2, _bytes, _hashes):
+    rows, cols = results1.shape[:2]
+    fig, axes = plt.subplots(rows, cols, sharex=True, sharey=True)
+    plt.suptitle("Each graph shows a sorted list of finish times for 224 processes")
+
+    for x in range(rows):
+        for y in range(cols):
+            plt.sca(axes[x, y])
+            plt.plot(results1[x, y], label="Get")
+            plt.plot(results2[x, y], label="No get")
+            axes[x, y].set_title(f"{_units(_bytes[x])}B, {_hashes[y]} hashes")
+            plt.legend()
+            if y == 0: axes[x, y].set_ylabel("Time [s]")
+            if x == rows - 1: axes[x, y].set_xlabel("Ray task [#]")
 
     plt.show()
 
     
 if __name__ == "__main__":
-    _bytes  = [2**x for x in [12, 14, 16, 18, 20]]
-    _hashes = [2**x for x in [12, 19, 20]]
+    # NOTE: Delete the savefile to force a rerun of the experiments
+    savefile = "npz/timelines.npz"
+    try:
+        timelines = np.load(savefile)
+        results_get   = timelines['arr_0']
+        results_noget = timelines['arr_1']
+        _bytes        = timelines['arr_2']
+        _hashes       = timelines['arr_3']
+    except: 
+        _bytes  = [2**x for x in [19, 20, 21, 22, 23, 24]]
+        _hashes = [2**x for x in [12, 19, 20]]
+        results_get = experiment(_bytes, _hashes, get=True)
+        results_noget = experiment(_bytes, _hashes, get=False)
+        np.savez(savefile, results_get, results_noget, _bytes, _hashes)
 
-    #
-    # NOTE: uncomment these 2 lines to run the experiments, 
-    #       but also comment out the 4 lines below
-    #
-    # results = experiment(_bytes, _hashes)
-    # np.savez("timelines.npz", results, _bytes, _hashes)
+    # plot_grid(results, _bytes, _hashes)
+    plot_two_grids(results_get, results_noget, _bytes, _hashes)
 
-    timelines = np.load("timelines.npz")
-    results = timelines['arr_0']
-    _bytes  = timelines['arr_1']
-    _hashes = timelines['arr_2']
-
-    plot_grid(results, _bytes, _hashes)
